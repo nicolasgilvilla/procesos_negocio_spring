dd07a2c2742a91e6060d71bcaa9ae01b8254717a
Nicolas Gil <nggilv@ufpso.edu.co>
2022-09-15 19:46:09 -0500

    First Commit

---
+++ src/test/java/com/procces/business/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procces.business.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
